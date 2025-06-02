bfc0c7188ae5d99697a391128fce30dd1ed6cf2f
SteveBartmoss <za19012397@zapopan.tecmm.edu.mx>
2025-06-01 19:11:39 -0600

    feat: Se agrego un componente para mostrar el menu superior del notepad

---
+++ src/components/menubar/MenuBar.jsx
@@ -0,0 +1,13 @@
+import "./menubar.css";
+
+export function MenuBar(){
+    return(
+        <div className="main-div-menu">
+            <ul>
+                <li>File</li>
+                <li>Edit</li>
+                <li>Help</li>
+            </ul>
+        </div>
+    )
+}
