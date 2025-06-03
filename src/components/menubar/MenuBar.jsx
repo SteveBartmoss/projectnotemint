221cc80766b3a3290a609346627254c752d8b028
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-06-03 14:38:37 -0600

    feat: Se agrego estructura para la simulacion de la ventana de un menu list

--- src/components/menubar/MenuBar.jsx
+++ src/components/menubar/MenuBar.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import "./menubar.css";
 
-export function MenuBar(){
-    return(
+export function MenuBar() {
+
+    const [collapse, setCollapse] = useState(false)
+
+
+    return (
         <div className="main-div-menu">
-            <ul>
-                <li>File</li>
-                <li>Edit</li>
-                <li>Help</li>
-            </ul>
+            <div className="header-div">
+                <ul>
+                    <li onClick={() => setCollapse(!collapse) }>File</li>
+                    <li>Edit</li>
+                    <li>Help</li>
+                </ul>
+            </div>
+            <div className={collapse ? 'list-div-open' : 'list-div-close'}>
+                <p>
+                    Prueba
+                </p>
+            </div>
         </div>
     )
 }
